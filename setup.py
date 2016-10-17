461169c63cf2174e016901bf8fa6f8c0426a6450
yang0110 <kaige.yang0110@gmail.com>
2016-10-17 22:20:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='comPy',
 	  version='0.1',
 	  description='communication system and signal processing library',
-	  url='pp',
+	  url='https://github.com/yang0110/comPy',
 	  author='Kaige Yang',
 	  license='MIT',
 	  packages=['comPy'],
