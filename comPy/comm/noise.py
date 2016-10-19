cdfc8aee07ec8056d70804e1b548721081b0137b
yang0110 <kaige.yang0110@gmail.com>
2016-10-19 18:38:56 +0100

    Add files via upload

---
+++ comPy/comm/noise.py
@@ -0,0 +1,15 @@
+----------------
+This file contains noise generators
+wgn
+
+-----------------------------------
+import numpy as np 
+
+def wgn(a,b,n):
+	noise=np.random.normal(a,b,n)
+	return noise
+	
+
+# a is the mean of the normal distribution 
+# b is the standard deviation of the normal distribution
+# n is the number of elements of noise
