e5c18fdacbea032094ffd4085017cba428662ade
yang0110 <kaige.yang0110@gmail.com>
2016-10-20 18:50:41 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ from distutils.core import setup
 
 setup(name='comPy',
 
-	  version='0.4',
+	  version='0.7',
 	  description='communication system and signal processing library',
 	  url='https://github.com/yang0110/comPy',
 	  author='Kaige Yang',
 	  license='MIT',
 	  author_email='kaige.yang0110@gmail.com',
 	  packages=['comPy'],
-	  download_url='https://github.com/yang0110/comPy/tarball/0.4',
+	  download_url='https://github.com/yang0110/comPy/tarball/0.7',
 	  
 
 	 
@@ -21,7 +21,7 @@ setup(name='comPy',
 
 DESCRIPTION = 'Communication signal system and signal processing  with Python'
 
-LONG_DESCRIPTION = open('README.py').read()
+
 
 MAINTAINER= 'Kaige Yang'
 
